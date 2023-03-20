b901c8fee6538966f1d5053c4ccf1392c6856c4b
Ebrahim <ebrahim.ghanem123@gmail.com>
2023-03-20 16:14:29 -0700

    automated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main fun it prints the text
+ * Return: nothing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
