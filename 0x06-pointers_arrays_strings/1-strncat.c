3d4555eb2cc54f2c83d7ddf4b549eec5943504e1
Ebrahim <ebrahim.ghanem123@gmail.com>
2023-03-29 11:55:11 -0700

    automated bu bash

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,30 @@
 #include "main.h"
 /**
- * _stract - concat on 32 strngs
- * @dest: strinh
- * @src: source string
- * return: stuff
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0;
-	int srclen = 0;
 	int i;
-	for (i = 0; dest[i] != '\0')
-		destlen++;
-	for (i = 0; src[i] != '\0')
-		srclen++;
-	for (i = 0; i < srclen; i++)
-		dest[destlen + i] = src[i];
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
